aed8c8c2095bbb391af1f00cc53b9095ac60aa28
anastasia <nety_nechego@mail.ru>
2024-11-12 21:19:55 +0300

    kek

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -3,5 +3,5 @@ package helloworld
 // This is your first homework!
 // Take a look at tests to get an idea of what this function should do.
 func HelloWorld() string {
-	return ""
+	return "Hello, World!"
 }
